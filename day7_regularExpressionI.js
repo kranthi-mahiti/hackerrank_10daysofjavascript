c2de4e5af526fe164c1e67e8751d512e09db9ef9
unknown <shirley.mramirez@yahoo.com>
2018-07-31 10:16:17 -0700

    regular expressionII

--- day7_regularExpressionI.js
+++ day7_regularExpressionI.js
@@ -59,9 +59,13 @@ function regexVar() {
      * Declare a RegExp object variable named 're'
      * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
      */
+    // .* will match 0 or more of any character.
+    // \1 is a reference to the first capture group (denoted by parentheses). 
+
     var re = /^([aeiou]).*\1$/i;
 
     /*
      * Do not remove the return statement
      */
     return re;
+}
