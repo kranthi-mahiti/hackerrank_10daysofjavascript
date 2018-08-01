bcd19da76228366548d9fba81b20992e719e6f08
unknown <shirley.mramirez@yahoo.com>
2018-07-31 19:03:39 -0700

    partial of binaryCalculator

---
+++ day9_binaryCalculator/js/binaryCalculator.js
@@ -0,0 +1,33 @@
+let result = document.getElementById('res');
+
+function button0() {
+    result.innerHTML += 0;
+}
+
+function button1() {
+    result.innerHTML += 1;
+}
+
+function buttonClear() {
+    result.innerHTML = '';
+}
+
+function buttonSum() {
+    result.innerHTML += '+';
+}
+
+function buttonSub() {
+    result.innerHTML += '-';
+}
+
+function buttonMul() {
+    result.innerHTML += '*';
+}
+
+function buttonDiv() {
+    result.innerHTML += '/';
+}
+
+
+
+
